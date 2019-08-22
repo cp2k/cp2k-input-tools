596333c7b6e18f1b51ed51fcae8cf41f8b3dfb61
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-08-22 17:34:45 +0200

    make it flake8 compat and pass along a filehandle

--- parse_cp2k_input.py
+++ parse_cp2k_input.py
@@ -6,118 +6,152 @@ import xml.etree.ElementTree as ET
 
 import transitions
 
+
 class TokenizerError(Exception):
     pass
+
 
 class UnterminatedStringError(TokenizerError):
     pass
 
+
 class InvalidTokenCharError(TokenizerError):
     pass
 
-class CP2KInputTokenizer(transitions.Machine):
-    def begin_basic_token(self, _, idx):
-        self._current_token_start = idx
 
-    def end_basic_token(self, _, idx):
+class CP2KInputTokenizer(transitions.Machine):
+    def begin_basic_token(self, _, colnr):
+        self._current_token_start = colnr
+
+    def end_basic_token(self, _, colnr):
         # the end idx follows the python-default of specifying ranges,
         # since this is triggered on the character after, using idx is correct
-        self._tokens += [(self._current_token_start, idx)]
+        self._tokens += [(self._current_token_start, colnr)]
 
-    def begin_string_token(self, content, idx):
-        self._current_token_start = idx
-        self._tracking_quote_char = content[idx]
+    def begin_string_token(self, content, colnr):
+        self._current_token_start = colnr
+        self._tracking_quote_char = content[colnr]
 
-    def end_string_token(self, content, idx):
+    def end_string_token(self, content, colnr):
         # this is trigger ON the encounter of the string token, while the
         # end of the basic token is determined by the character that follows
-        self._tokens += [(self._current_token_start, idx+1)]
+        self._tokens += [(self._current_token_start, colnr + 1)]
 
-    def unterminated_string(self, _, idx):
-        raise UnterminatedStringError(f"unterminated string detected, starting at {self._current_token_start}")
+    def unterminated_string(self, _, colnr):
+        raise UnterminatedStringError(
+            f"unterminated string detected, starting at {self._current_token_start}"
+        )
 
-    def invalid_token_char(self, _, idx):
-        raise InvalidTokenCharError(f"invalid character detected at index {idx} (token started in {self._current_token_start})")
+    def invalid_token_char(self, _, colnr):
+        raise InvalidTokenCharError(
+            f"invalid character detected at index {colnr} (token started in {self._current_token_start})"
+        )
 
-    def is_not_escaped(self, content, idx):
-        if idx > 0:
-            return content[idx-1] != '\\'
+    def is_not_escaped(self, content, colnr):
+        if colnr > 0:
+            # possible to do: account for multiple escapes
+            return content[colnr - 1] != "\\"
 
         return True
 
-    def is_matching_quote(self, content, idx):
-        return self._tracking_quote_char == content[idx]
+    def is_matching_quote(self, content, colnr):
+        return self._tracking_quote_char == content[colnr]
 
     def __init__(self):
-        super().__init__(self,
-            initial = "lookout",
-
-            states = [
+        super().__init__(
+            self,
+            initial="lookout",
+            states=[
                 transitions.State(name="lookout"),
-                transitions.State(name="basic_token", on_enter=["begin_basic_token"], on_exit=["end_basic_token"]),
-                transitions.State(name="string_token", on_enter=["begin_string_token"], on_exit=["end_string_token"]),
-                transitions.State(name="comment", on_enter=["begin_basic_token"], on_exit=["end_basic_token"]),
-                ],
-            transitions = [
+                transitions.State(
+                    name="basic_token",
+                    on_enter=["begin_basic_token"],
+                    on_exit=["end_basic_token"],
+                ),
+                transitions.State(
+                    name="string_token",
+                    on_enter=["begin_string_token"],
+                    on_exit=["end_string_token"],
+                ),
+                transitions.State(
+                    name="comment",
+                    on_enter=["begin_basic_token"],
+                    on_exit=["end_basic_token"],
+                ),
+            ],
+            transitions=[
                 # start parsing a token:
-                {'trigger': 'token_char', 'source': 'lookout', 'dest': 'basic_token'},
+                {"trigger": "token_char", "source": "lookout", "dest": "basic_token"},
                 # ... unless we're already parsing a token or inside a string or comment
-                {'trigger': 'token_char',
-                 'source': ['basic_token', 'string_token', 'comment'],
-                 'dest': None },
-
+                {
+                    "trigger": "token_char",
+                    "source": ["basic_token", "string_token", "comment"],
+                    "dest": None,
+                },
                 # '/" initiate strings
-                { 'trigger': 'quote_char', 'source': 'lookout', 'dest': 'string_token' },
-                { 'trigger': 'quote_char', 'source': 'string_token', 'dest': 'lookout',
-                  'conditions': ['is_not_escaped', 'is_matching_quote'] },
-
+                {"trigger": "quote_char", "source": "lookout", "dest": "string_token"},
+                {
+                    "trigger": "quote_char",
+                    "source": "string_token",
+                    "dest": "lookout",
+                    "conditions": ["is_not_escaped", "is_matching_quote"],
+                },
                 # a '!' initiates a comment (and terminates a token if necessary)
-                { 'trigger': 'comment_char', 'source': ['lookout', 'basic_token'], 'dest': 'comment' },
+                {
+                    "trigger": "comment_char",
+                    "source": ["lookout", "basic_token"],
+                    "dest": "comment",
+                },
                 # ... unless inside a single or double quoted string, where it is consumed:
-                {'trigger': 'comment_char', 'source': 'string_token', 'dest': None},
-
+                {"trigger": "comment_char", "source": "string_token", "dest": None},
                 # whitespace terminates a basic token
-                {'trigger': 'ws_char', 'source': 'basic_token', 'dest': 'lookout' },
+                {"trigger": "ws_char", "source": "basic_token", "dest": "lookout"},
                 # ... and is consumed in all other cases
-                {'trigger': 'ws_char',
-                 'source': ['lookout', 'string_token', 'comment'],
-                 'dest': None, },
-
+                {
+                    "trigger": "ws_char",
+                    "source": ["lookout", "string_token", "comment"],
+                    "dest": None,
+                },
                 # single/double quotes are not allowed in a basic token:
-                { 'trigger': 'quote_char', 'source': 'basic_token',
-                  'before': 'invalid_token_char', 'dest': None },
-
-                { 'trigger': 'nl_char',
-                  'source': ['basic_token', 'comment'],
-                  'dest': 'lookout'},
-                { 'trigger': 'nl_char',
-                  'source': 'lookout',
-                  'dest': None},
-                { 'trigger': 'nl_char',
-                  'source': 'string_token',
-                  'before': 'unterminated_string',
-                  'dest': None },
-
-                ]
+                {
+                    "trigger": "quote_char",
+                    "source": "basic_token",
+                    "before": "invalid_token_char",
+                    "dest": None,
+                },
+                {
+                    "trigger": "nl_char",
+                    "source": ["basic_token", "comment"],
+                    "dest": "lookout",
+                },
+                {"trigger": "nl_char", "source": "lookout", "dest": None},
+                {
+                    "trigger": "nl_char",
+                    "source": "string_token",
+                    "before": "unterminated_string",
+                    "dest": None,
+                },
+            ],
         )
 
         self._tracking_quote_char = None
         self._current_token_start = 0
         self._tokens = []
 
-    def token_iter(self, content):
+    def token_iter(self, fhandle):
         char_map = {
-                " ":  self.ws_char,
-                "\t": self.ws_char,
-                "!":  self.comment_char,
-                "#":  self.comment_char,
-                "'":  self.quote_char,
-                "\"": self.quote_char,
-            }
+            " ": self.ws_char,
+            "\t": self.ws_char,
+            "!": self.comment_char,
+            "#": self.comment_char,
+            "'": self.quote_char,
+            '"': self.quote_char,
+            "\n": self.nl_char,
+        }
 
-        for linenr, line in enumerate(content.splitlines()):
-            for idx, char in enumerate(line):
-                char_map.get(char, self.token_char)(line, idx)
+        for linenr, line in enumerate(fhandle):
+            for colnr, char in enumerate(line):
+                char_map.get(char, self.token_char)(line, colnr)
 
             self.nl_char(line, len(line))
             if self._tokens:
@@ -134,13 +168,14 @@ def parse_dt_kw(element):
 
 
 TYPE_PARSERS = {
-    'integer': lambda e: {"type": "integer", "n_var": int(e.find("./N_VAR").text)},
-    'keyword': parse_dt_kw,
-    'logical': lambda e: {"type": "logical", "n_var": int(e.find("./N_VAR").text)},
-    'real': lambda e: {"type": "real", "n_var": int(e.find("./N_VAR").text)},
-    'string': lambda e: {"type": "string", "n_var": int(e.find("./N_VAR").text)},
-    'word': lambda e: {"type": "word", "n_var": int(e.find("./N_VAR").text)},
+    "integer": lambda e: {"type": "integer", "n_var": int(e.find("./N_VAR").text)},
+    "keyword": parse_dt_kw,
+    "logical": lambda e: {"type": "logical", "n_var": int(e.find("./N_VAR").text)},
+    "real": lambda e: {"type": "real", "n_var": int(e.find("./N_VAR").text)},
+    "string": lambda e: {"type": "string", "n_var": int(e.find("./N_VAR").text)},
+    "word": lambda e: {"type": "word", "n_var": int(e.find("./N_VAR").text)},
 }
+
 
 class ParserError(Exception):
     pass
@@ -174,20 +209,22 @@ def string_kw_converter(string):
     return string.strip("'\"")
 
 
-FORTRAN_REAL = re.compile(r'(\d*\.\d+)[dD]([-+]?\d+)')
+FORTRAN_REAL = re.compile(r"(\d*\.\d+)[dD]([-+]?\d+)")
+
 
 def real_kw_converter(string):
     return float(FORTRAN_REAL.sub(r"\1e\2", string))
 
 
 KW_VALUE_CONVERTERS = {
-    'keyword': string_kw_converter,
-    'logical': bool_kw_converter,
-    'integer': int,
-    'real': real_kw_converter,
-    'word': string_kw_converter,
-    'string': string_kw_converter,
+    "keyword": string_kw_converter,
+    "logical": bool_kw_converter,
+    "integer": int,
+    "real": real_kw_converter,
+    "word": string_kw_converter,
+    "string": string_kw_converter,
 }
+
 
 def parse_tokens(element, tokens, key=None):
     if not key:
@@ -221,7 +258,9 @@ def parse_tokens(element, tokens, key=None):
 
     if not tokens:
         if not lone_keyword_value:
-            raise InvalidParameterError(f"the keyword '{key}' expects at least one value")
+            raise InvalidParameterError(
+                f"the keyword '{key}' expects at least one value"
+            )
 
         tokens = [lone_keyword_value]
 
@@ -237,21 +276,29 @@ def parse_tokens(element, tokens, key=None):
     for token in tokens:
         if token.startswith("["):
             if not default_unit:
-                raise InvalidParameterError(f"unit specified for value in keyword '{key}', but no default unit available")
-            current_unit = token.strip('[]')
+                raise InvalidParameterError(
+                    f"unit specified for value in keyword '{key}', but no default unit available"
+                )
+            current_unit = token.strip("[]")
             continue
 
         value = value_converter(token)
 
-        assert current_unit == default_unit, f"unit conversion not (yet) implemented (keyword: '{key}')"
+        assert (
+            current_unit == default_unit
+        ), f"unit conversion not (yet) implemented (keyword: '{key}')"
 
         values += [value]
 
     if not values:
-        raise InvalidParameterError(f"the keyword '{key}' expects at least one value, only a unit spec was given")
+        raise InvalidParameterError(
+            f"the keyword '{key}' expects at least one value, only a unit spec was given"
+        )
 
     if (type_info["n_var"] > 0) and (type_info["n_var"] != len(values)):
-        raise InvalidParameterError(f"the keyword '{key}' expects exactly {type_info['n_var']} values, {len(values)} were given")
+        raise InvalidParameterError(
+            f"the keyword '{key}' expects exactly {type_info['n_var']} values, {len(values)} were given"
+        )
 
     # simplify the value if only one is given/requested
     if len(values) == 1:
@@ -266,6 +313,7 @@ def parse_tokens(element, tokens, key=None):
         "type": type_info,
         "values": values,
     }
+
 
 class CP2KInputParser:
     def __init__(self, xmlspec):
@@ -285,11 +333,13 @@ class CP2KInputParser:
             if not tokens:
                 continue  # go to next if the comment was the only token
 
-            if tokens[0].startswith('&'):
+            if tokens[0].startswith("&"):
                 section_name = tokens.pop(0)[1:].upper()
 
                 if section_name == "END":
-                    if tokens and tokens[0].upper() not in [e.text for e in nodes[-1].iterfind("./NAME")]:
+                    if tokens and tokens[0].upper() not in [
+                        e.text for e in nodes[-1].iterfind("./NAME")
+                    ]:
                         raise SectionMismatchError()
 
                     if len(tokens) > 1:
@@ -304,7 +354,9 @@ class CP2KInputParser:
                     if section_name not in [e.text for e in sec.iterfind("./NAME")]:
                         continue
 
-                    nodes += [sec]  # add the current XML section node to the stack of nodes
+                    nodes += [
+                        sec
+                    ]  # add the current XML section node to the stack of nodes
 
                     repeats = True if sec.get("repeats") == "yes" else False
 
@@ -316,7 +368,9 @@ class CP2KInputParser:
                         treerefs += [treerefs[-1][section_name][-1]]
                     else:
                         if section_name in treerefs[-1]:
-                            raise InvalidNameError(f"the section '{section_name}' can only be mentioned once")
+                            raise InvalidNameError(
+                                f"the section '{section_name}' can only be mentioned once"
+                            )
 
                         treerefs[-1][section_name] = {}
                         treerefs += [treerefs[-1][section_name]]
@@ -325,7 +379,9 @@ class CP2KInputParser:
                     param = sec.find("./SECTION_PARAMETERS")
                     if param:  # validate the section parameter like a kw datatype
                         # there is no way we get a second section parameter, assign directly
-                        treerefs[-1]["_"] = parse_tokens(param, tokens, "SECTION_PARAMETERS")["values"]
+                        treerefs[-1]["_"] = parse_tokens(
+                            param, tokens, "SECTION_PARAMETERS"
+                        )["values"]
 
                     break
                 else:
@@ -346,7 +402,9 @@ class CP2KInputParser:
                             treerefs[-1][data["name"]] += [data["values"]]
                         else:
                             if data["name"] in treerefs[-1]:
-                                raise InvalidNameError(f"the keyword '{token_name}' can only be mentioned once")
+                                raise InvalidNameError(
+                                    f"the keyword '{token_name}' can only be mentioned once"
+                                )
 
                             treerefs[-1][data["name"]] = data["values"]
 
@@ -373,14 +431,18 @@ class CP2KInputParser:
                         treerefs[-1]["*"] += [data["values"]]
                     else:
                         if "*" in treerefs[-1]:
-                            raise InvalidNameError(f"the default keyword in section '...' can only be used once")
+                            raise InvalidNameError(
+                                f"the default keyword in section '...' can only be used once"
+                            )
                         treerefs[-1]["*"] = data["values"]
 
         return tree
 
+
 parser = CP2KInputParser("cp2k_input.xml")
-#parser.parse("    \t FOO \t  ' \\' \"  ! baz'  BAR   .13  ! comment  y\n abc")
+# parser.parse("    \t FOO \t  ' \\' \"  ! baz'  BAR   .13  ! comment  y\n abc")
 with open("test01.inp", "r") as fhandle:
-    tree = parser.parse(fhandle.read())
+    tree = parser.parse(fhandle)
     import json
+
     print(json.dumps(tree, indent=2))
