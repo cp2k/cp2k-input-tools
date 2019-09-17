47ba14c71f7e964c8957be2769edd1d7678f9f63
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-09-17 14:58:07 +0200

    tests: also test cp2klint

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -38,3 +38,18 @@ def test_tocp2k_json(script_runner):
     assert ret.stderr == ""
 
     assert "&GLOBAL" in ret.stdout
+
+
+def test_cp2klint(script_runner):
+    ret = script_runner.run("cp2klint", str(TEST_DIR.joinpath("inputs/test01.inp")))
+
+    assert ret.success
+    assert ret.stderr == ""
+
+
+def test_cp2klint_error(script_runner):
+    ret = script_runner.run("cp2klint", str(TEST_DIR.joinpath("inputs/error_nvar.inp")))
+
+    assert not ret.success
+    assert "Syntax error" in ret.stdout
+    assert "invalid values for keyword: A" in ret.stdout
