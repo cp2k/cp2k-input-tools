e54bc754c1e9662ec60596e0d5da90e827a30774
Andreas Knüpfer <knue@posteo.de>
2026-01-26 09:12:33 +0100

    Update cp2k_input_tools/parser.py
    Co-authored-by: Tiziano Müller <tm@dev-zero.ch>

--- cp2k_input_tools/parser.py
+++ cp2k_input_tools/parser.py
@@ -67,7 +67,7 @@ class CP2KInputParser:
         :param key_trafo: A function object used for mangling key names, must treat input case-insensitive
         """
 
-        if None == xmlspec:
+        if xmlspec:
             xmlspec = DEFAULT_CP2K_INPUT_XML
 
         # schema:
